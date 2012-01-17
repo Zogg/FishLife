af0a681206549c97f12a65933cfd2a1efed6e5bb
Zogg <none@none>
2012-01-17 03:35:43 +0200

    fish physics in beta stage

--- fish.py
+++ fish.py
@@ -1,3 +1,5 @@
+from math import sin, cos, radians
+
 from kivy.uix.image import Image
 from kivy.uix.scatter import Scatter
 from kivy.core.window import Window
@@ -16,12 +18,14 @@ class Fish(Scatter):
     calories_consumption = 7
     
     def __init__(self, image = "images/fish.png", **kwargs):
-        self.direction = Vector(-1, -1)
+        self.direction = Vector(-1, 0)
         self.speed = 0
         
         self.size = (48,48)
         self.source = image
         self.image = Image(source=image, allow_stretch=True, size=self.size)
+
+        self.target_pos = self.center
         
         super(Fish, self).__init__(**kwargs)
         self.add_widget(self.image)
@@ -52,23 +56,42 @@ class Fish(Scatter):
             self.obese_lvl += 1
             self.image.size = (self.image.width + self.image.width * (1.0 / self.obese_lvl), self.image.height + self.image.height * (1.0 / self.obese_lvl))
             self.size = self.image.size
-    
+            
+    def swim(self, dt):
+        anim = Animation(center=self.target_pos, d=0.1)
+        anim.start(self)
+        
     def on_death(self):
         self.alive = False
         self.active = False
         print "dead"
             
+    def on_touch_down(self, touch):
+        Clock.schedule_interval(self.swim, 0.1)
+        
     def on_touch_move(self, touch):
-        self.center = (touch.x, touch.y)
         #self.direction = (touch.dsx, touch.dsy)
-        #self.rotation = self.direction.angle((touch.dsx, touch.dsy))
         angle = self.direction.angle((touch.dsx, touch.dsy))
-        self.speed += Vector((0,0)).distance(touch.dsx, touch.dsy)
-        if angle < 0:
-            angle = 180 + angle
-        anim = Animation(rotation=angle, d=0.1)
-        anim.start(self)
-        print touch.dsx
-        #self.rotation = angle
-    
 
+        if angle < 0:
+            angle = 360 + angle
+        angle = 270 - angle
+        
+        # TODO: solve facing glitch problem with Clock, which sets facing_change cooldown timer for half a sec
+
+        #self.center = (touch.x + sin(radians(angle)) * self.speed * 10, touch.y + cos(radians(angle)) * self.speed * 10)
+        self.target_pos = (touch.x, touch.y)
+        
+    def on_touch_up(self, touch):
+        Clock.unschedule(self.swim)
+        
+        self.speed = Vector((0,0)).distance((touch.dsx, touch.dsy)) * 5000
+        
+        angle = self.direction.angle((touch.dsx, touch.dsy))
+        if angle < 0:
+            angle = 360 + angle
+        angle = 270 - angle
+
+        anim = Animation(center=(self.target_pos[0] + sin(radians(angle)) * self.speed,self.target_pos[1] - cos(radians(angle)) * self.speed), t="out_cubic", d=0.6)
+        anim.start(self)
+        
